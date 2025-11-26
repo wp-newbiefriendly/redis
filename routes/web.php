f6f6b4cca6071fb68845596fb19180e714a033d1
wp-newbiefriendly <locokoks@gmail.com>
2025-11-26 01:36:22 +0100

    #28.4 - Installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductsController;
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/',[HomeController::class,'index']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::view('/products', 'add-products');
 Route::post('/products/create', [ProductsController::class, 'create']);
 Route::get('/products/flush', [ProductsController::class, 'flush'])->name('flush');
+
+require __DIR__.'/auth.php';
