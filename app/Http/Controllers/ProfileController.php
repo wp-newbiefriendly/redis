f5b413ec209d270145b32f2c2124a629a62bed49
wp-newbiefriendly <locokoks@gmail.com>
2025-11-28 07:20:42 +0100

    #28.15 - Image validation/remove/update/storage

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -24,7 +25,18 @@ class ProfileController extends Controller
 
     public function changeAvatar(NewAvatarRequest $request)
     {
-    dd($request->validated());
+        $avatar = Auth::user()->avatar;
+
+        if ($avatar !== null) {
+            File::delete("storage/images/avatars/{$avatar}");
+        }
+
+        $filePath = $request->file('profile_image')
+            ->store('images/avatars', 'public');
+
+        $name = basename($filePath);
+
+        Auth::user()->update(['avatar' => $name]);
     }
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
